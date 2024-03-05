fda36e8d1903dbdd69345072db402d6886c73701
adrianp <adrian.popa.gh@gmail.com>
2024-03-05 18:11:03 +0200

    Create dir as parent, not inside child

--- split-dir-by-date.pl
+++ split-dir-by-date.pl
@@ -4,11 +4,13 @@ use warnings;
 use File::Find;
 use File::Copy;
 use POSIX qw(strftime);
+use File::Basename;
+use Cwd 'realpath';
 
 my $dir = shift or die "Usage: $0 DIRECTORY\n";
 
 my %dates;
-
+my %created_dirs;
 # collect dates
 find(sub {
     return unless -f; # Skip directories
@@ -27,8 +29,18 @@ find(sub {
     return unless -f; # Skip directories
     my $mtime = (stat)[9]; # modification time
     my $date = strftime('%Y-%m-%d', localtime($mtime)); 
-    my $new_dir = "$dir-$date";
-    mkdir $new_dir unless -e $new_dir; # Create directory if it doesn't exist
+    my $parent_dir = dirname($dir);
+    my $absolute_parent = realpath($parent_dir);
+    my $new_dir = "$absolute_parent-$date";
+    if (-e $new_dir){
+	print(" >> Not creating $new_dir, because it exists!\n") if not defined $created_dirs{$new_dir};
+	$created_dirs{$new_dir} = 1;
+    }
+    else {
+    	mkdir $new_dir or die $!;
+	print(" >> Created $new_dir\n");
+	$created_dirs{$new_dir} = 1;
+    }
     print("Moving $_ to $new_dir\n");
     move($_, $new_dir) or die "Failed to move $_ to $new_dir: $!\n";
 }, $dir);
