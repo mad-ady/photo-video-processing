e7c048ab85adc94678a336774fb4e143f2307099
adrianp <adrian.popa.gh@gmail.com>
2024-11-07 23:20:24 +0200

    Automatically delete original photo after successful downscale

--- downscale-pictures-to-4k.sh
+++ downscale-pictures-to-4k.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 # Usage: downscale-image-to-4k.sh IMG*.jpg [IMG*.jpg...]
 
+DELETEORIGINAL=1
+
 for srcfile
 do
   case "$srcfile" in
@@ -23,6 +25,14 @@ do
       if [ ! -r "$basedir/${basefile}_4k.jpg" ]; then
           echo "Downsampling $srcfile from $height to ${basefile}_4k.jpg (2250)";
           convert "$srcfile" -filter Lanczos -resize 4000x2250 "$basedir/${basefile}_4k.jpg"
+          FILESIZE=$(stat -c%s "$basedir/${basefile}_4k.jpg")
+          if [ "$FILESIZE" -gt "2000000" -a "$DELETEORIGINAL" -gt "0" ]; then
+              #output is >2MB, we assume it's fine
+              echo "Deleting $srcfile"
+              rm -f "$srcfile";
+          else
+              echo "Warning! Output looks too small: $FILESIZE";
+          fi
       else
            echo "${basefile}_4k.jpg already exists, skipping";
       fi
